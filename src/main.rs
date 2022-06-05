655bdfb4ef8705f0e86e94e5bf3b7bc5c59c7b3d
Roy <roybcx@pm.me>
2022-06-05 23:44:06 +0300

    adding tests

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,12 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "graden")]
 struct Opt {
-    #[structopt(parse(from_os_str), short = "p", long, env)]
+    #[structopt(
+        parse(from_os_str), 
+        short = "p", 
+        long, 
+        env
+    )]
     garden_path: Option<PathBuf>,
 
     #[structopt(subcommand)]
@@ -19,7 +24,10 @@ struct Opt {
 enum Command {
     /// Write something in your garden
     Write {
-        #[structopt(short, long)]
+        #[structopt(
+            short, 
+            long
+        )]
         /// Optionally set a title for what you're going to write about
         title: Option<String>,
     },
@@ -34,22 +42,20 @@ fn get_default_garden_dir() -> Result<PathBuf> {
 }
 
 fn main() -> Result<()> {
-    
     color_eyre::install()?;
-
     let opt: Opt = Opt::from_args();
-    
     dbg!(&opt);
-    
-    let garden_path: PathBuf = match opt.garden_path {
-        Some(pathbuf) => Ok(pathbuf),
-        None => get_default_garden_dir()
-            .wrap_err("`garden_path` was not supplied"),
-    }?;
+
+    let garden_path: PathBuf = 
+        match opt.garden_path {
+            Some(pathbuf) => Ok(pathbuf),
+            None => 
+                get_default_garden_dir()
+                    .wrap_err("`garden_path` was not supplied"),
+        }?;
 
     match opt.cmd {
-        Command::Write { 
-            title 
-        } => write(garden_path, title),
+        Command::Write { title } => 
+            write(garden_path, title),
     }
 }
